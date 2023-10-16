5d28d3848e323bd5637c97d8ca5d619aa62b91bd
namishagurunani <126158413+namishagurunani@users.noreply.github.com>
2023-10-16 16:15:25 +0530

    Update script.js

--- Sort-Table/script.js
+++ Sort-Table/script.js
@@ -1,13 +1,19 @@
 document.addEventListener("DOMContentLoaded", function() {
     const customerTable = document.getElementById("customerTable");
     const sortButton = document.getElementById("sortButton");
+    let ascendingOrder = true;
 
     sortButton.addEventListener("click", function() {
         const rows = Array.from(customerTable.rows).slice(1); // Skip the header row
         rows.sort((a, b) => {
             const nameA = a.cells[0].textContent.toLowerCase();
             const nameB = b.cells[0].textContent.toLowerCase();
-            return nameA.localeCompare(nameB);
+
+            if (ascendingOrder) {
+                return nameA.localeCompare(nameB);
+            } else {
+                return nameB.localeCompare(nameA);
+            }
         });
 
         // Clear the table
@@ -17,5 +23,9 @@ document.addEventListener("DOMContentLoaded", function() {
 
         // Rebuild the table with sorted data
         rows.forEach(row => customerTable.tBodies[0].appendChild(row));
+
+        // Toggle sorting order
+        ascendingOrder = !ascendingOrder;
+        sortButton.textContent = ascendingOrder ? "Sort Ascending" : "Sort Descending";
     });
 });
